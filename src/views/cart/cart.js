25db08914dc3101b557ea9c8e2fc598a28cfb727
[SW4/2반] 정웅비 <zo0qzzz@gmail.com>
2023-04-23 03:23:30 +0900

    cart 폴더 복구

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -1,8 +1,3 @@
-window.onLoad(() => {
-  const localStorageData = JSON.parse(localStorage.getItem('cart'));
-  localStorage.setItem('cart', localStorageData)
-})
-
 const inCart = document.querySelector('.inCart-productList-ul');
 const paymentInformation = document.querySelector('.paymentInformation');
 const button1 = document.querySelector('#button1');
@@ -11,8 +6,7 @@ const button2 = document.querySelector('#button2');
 const inCartList = [];
 
 for(i = 0; i < 1; i++) {
-  inCart.innerHTML += 
-  `
+  inCart.innerHTML += `
   <li class="inCart-productList-li">
     <div class="container-productList-checkbox">
       <input type="checkbox">
@@ -56,11 +50,55 @@ for(i = 0; i < 1; i++) {
     </div>
   </li>
   `
-}
   
-let data = [];
+}
 
-button1.addEventListener('click', (e) => {
+
+let localStorageData = JSON.parse(localStorage.getItem('cart'));
+
+if(localStorageData == null) {
+
+  const paymentInformationHTML = `
+  <div class="container-paymentInformation-subject">
+    <div class="paymentInformation-subject">결제 정보</div>
+  </div>
+  <div class="container-paymentInformation-productCount">
+    <div class="paymentInformation-productCount1">상품 수</div>
+    <div class="paymentInformation-productCount2">0</div>
+  </div>
+  <div class="container-paymentInformation-productPrice">
+    <div class="paymentInformation-productPrice1">상품 금액</div>
+    <div class="paymentInformation-productPrice2">0</div>
+  </div>
+  <div class="container-paymentInformation-deliveryFee">
+    <div class="paymentInformation-deliveryFee1">배송비</div>
+    <div class="paymentInformation-deliveryFee2">0</div>
+  </div>      
+  <div class="container-paymentInformation-totalPrice">
+    <div class="paymentInformation-totalPrice1">총 결제 금액</div>
+    <div class="paymentInformation-totalPrice2">0원</div>
+  </div>     
+  <div class="container-paymentInformation-paymentButton">
+    <button class="paymentInformation-paymentButton">구매하기</button>
+  </div>
+  `
+
+  paymentInformation.innerHTML = paymentInformationHTML;
+  
+  localStorageData = [];
+}
+
+
+button1.addEventListener('click', func1)
+
+button2.addEventListener('click', func2)
+
+let totalProductCount = 0;
+let totalProductPrice = 0;
+let deliveryFee = 0;
+let totalPrice = 0;
+
+function func1() {
   const data1 = { id: 1,
     name: '이상해씨',
     type: '풀',
@@ -68,50 +106,18 @@ button1.addEventListener('click', (e) => {
     count: 1,
   }
 
-  data.push(data1);
+  localStorageData.push(data1);
+  localStorage.setItem('cart', JSON.stringify(localStorageData));
 
-  localStorage.setItem('cart', JSON.stringify(data));
-})
+    localStorageData.forEach(item => {
+      const { price, count } = item;
+      
+      totalProductCount += count;
+      totalProductPrice += price;
+      totalPrice += count * price;
+    })
 
-button2.addEventListener('click', (e) => {
-  const data2 = {
-    id: 2,
-    name: '피카츄',
-    type: '전기',
-    price: 10000,
-    count: 2,  
-  }
-  
-  data.push(data2);
-  
-  localStorage.setItem('cart', JSON.stringify(data));
-})
-
-console.log(data)
-
-const localStorageData = JSON.parse(localStorage.getItem('cart'));
-
-function firstCart() {
-  const localStorageData = JSON.parse(localStorage.getItem('cart'));
-
-  localStorage.setItem('cart', localStorageData)
-}
-console.log(localStorageData);
-
-let totalProductCount = 0;
-let totalProductPrice = 0;
-let deliveryFee = 0;
-
-localStorageData.forEach(item => {
-  const { price, count } = item;
-  
-  totalProductCount += count;
-  totalProductPrice += price;
-})
-
-let totalPrice = totalProductPrice + deliveryFee;
-
-const paymentInformationHTML =
+  const paymentInformationHTML =
   `
   <div class="container-paymentInformation-subject">
     <div class="paymentInformation-subject">결제 정보</div>
@@ -137,4 +143,71 @@ const paymentInformationHTML =
   </div>
   `
 
-  paymentInformation.innerHTML = paymentInformationHTML
+  paymentInformation.innerHTML = paymentInformationHTML;
+
+  totalProductCount = 0;
+  totalProductPrice = 0;
+  deliveryFee = 0;
+  totalPrice = 0;
+}
+
+function func2() {
+  const data2 = {
+    id: 2,
+    name: '피카츄',
+    type: '전기',
+    price: 10000,
+    count: 1,  
+  }
+
+  if(localStorageData == null) {
+    localStorageData = [];
+  }
+
+  localStorageData.push(data2);
+  localStorage.setItem('cart', JSON.stringify(localStorageData));
+
+    localStorageData.forEach(item => {
+      const { price, count } = item;
+      
+      totalProductCount += count;
+      totalProductPrice += price;
+      totalPrice += count * price;
+    })
+
+  const paymentInformationHTML =
+  `
+  <div class="container-paymentInformation-subject">
+    <div class="paymentInformation-subject">결제 정보</div>
+  </div>
+  <div class="container-paymentInformation-productCount">
+    <div class="paymentInformation-productCount1">상품 수</div>
+    <div class="paymentInformation-productCount2">${totalProductCount}</div>
+  </div>
+  <div class="container-paymentInformation-productPrice">
+    <div class="paymentInformation-productPrice1">상품 금액</div>
+    <div class="paymentInformation-productPrice2">${totalProductPrice}</div>
+  </div>
+  <div class="container-paymentInformation-deliveryFee">
+    <div class="paymentInformation-deliveryFee1">배송비</div>
+    <div class="paymentInformation-deliveryFee2">${deliveryFee}</div>
+  </div>      
+  <div class="container-paymentInformation-totalPrice">
+    <div class="paymentInformation-totalPrice1">총 결제 금액</div>
+    <div class="paymentInformation-totalPrice2">${totalPrice}원</div>
+  </div>     
+  <div class="container-paymentInformation-paymentButton">
+    <button class="paymentInformation-paymentButton">구매하기</button>
+  </div>
+  `
+
+  paymentInformation.innerHTML = paymentInformationHTML;
+
+  totalProductCount = 0;
+  totalProductPrice = 0;
+  deliveryFee = 0;
+  totalPrice = 0;
+}
+
+
+  
