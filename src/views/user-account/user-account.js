1b0dd5e25baea207361cc1dd9cb839e2ed4f52f5
[SW4/1반] 이준미 <junmi950918@gmail.com>
2023-04-26 01:35:55 +0900

    비밀번호 8글자 이상인지 확인하는 코드 추가

--- src/views/user-account/user-account.js
+++ src/views/user-account/user-account.js
@@ -98,13 +98,27 @@ async function doCheckout(e) {
 }
 
 
+const passwordInput = document.querySelector("#account-password");
+const confirmPasswordInput = document.querySelector("#account-confirmPassword");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirmPassword");
+
+
 // 패스워드가 8글자인지 확인 
+passwordInput.addEventListener("input", function() {
+  console.log(passwordInput.value.length);
+  const passwordLength = passwordInput.value.length;
+
+  if(passwordLength < 9) {
+    password.classList.remove("block");
+  } else {
+    password.classList.add("block");
+  }
+})
 
 
 // 패스워드가 동일한지
-const passwordInput = document.querySelector("#account-password");
-const confirmPasswordInput = document.querySelector("#account-confirmPassword");
-const confirmPassword = document.querySelector(".error-M")
+
 
 confirmPasswordInput.addEventListener("input", function() {
   console.log(confirmPasswordInput.value);
