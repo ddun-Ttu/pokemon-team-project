8e3ac58e3206d868eb4308c79997deb5e9faff7a
[SW4/1반] 이준미 <junmi950918@gmail.com>
2023-04-26 02:49:04 +0900

    로그인 아이디에서 이메일로 수정

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,27 +1,27 @@
-// const passwordInput = document.querySelector("#login-password");
-// const loginInput = document.querySelector("#login-email");
+const passwordInput = document.querySelector("#login-password");
+const loginInput = document.querySelector("#login-email");
 
-// //에러 메세지
-// const errorMessage = document.createElement("p");
-// errorMessage.style.color ="red";
-// errorMessage.style.fontSize = "12px";
+//에러 메세지
+const errorMessage = document.createElement("p");
+errorMessage.style.color ="red";
+errorMessage.style.fontSize = "12px";
 
-// //이메일 형식이 맞는지 확인
-// loginInput.addEventListener("input", function () {
-//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//이메일 형식이 맞는지 확인
+loginInput.addEventListener("input", function () {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-//     if (!emailRegex.test(this.value)) {
-//       errorMessage.textContent = "이메일 형식에 맞게 작성해주세요";
-//       errorMessage.style.display = "block";
-//       loginInput.parentNode.insertBefore(errorMessage, loginInput.nextSibling);
-//     } 
-//     else {
-//       errorMessage.style.display = "none";
-//     }
-//     if ( loginInput.value.length === 0){
-//         errorMessage.style.display = "none";
-//     }
-//   });
+    if (!emailRegex.test(this.value)) {
+      errorMessage.textContent = "이메일 형식에 맞게 작성해주세요";
+      errorMessage.style.display = "block";
+      loginInput.parentNode.insertBefore(errorMessage, loginInput.nextSibling);
+    } 
+    else {
+      errorMessage.style.display = "none";
+    }
+    if ( loginInput.value.length === 0){
+        errorMessage.style.display = "none";
+    }
+  });
 
   // Input 값 확인하기
   const loginBut = document.querySelector("#login-but");
