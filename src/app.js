538154b8a52030a3ced557f1ef634e7e8cc57ee3
buyeolim <buyeolim@gmail.com>
2023-04-26 16:21:18 +0900

    루트 경로 및 포켓몬(상품) 주문, 결제 관련 페이지

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
 import path from "path";
+// 라우터
 import userRouter from "./routes/userRouter";
 import adminRouter from "./routes/adminRouter";
+import rootRouter from "./routes/rootRouter";
 
 const app = express();
 const logger = morgan("dev");
@@ -12,12 +14,15 @@ app.use(express.static(__dirname)); // 이미지 경로 고정
 app.use(express.static(__dirname + "/views")); // 화면 경로 고정
 
 app.use("/users", userRouter); // 사용자(개인) 페이지 관련 라우터
-app.use("/admins", adminRouter); // 관리자 페이지 관련 라우터
+app.use("/admins", adminRouter); // 사용자(개인) 페이지 관련 라우터
+app.use("/", rootRouter);
 
+/*
 app.use("/", (req, res) => {
   res.sendFile("main.html", {
     root: path.join(__dirname, "views/main"),
   });
 });
+*/
 
 export default app;
