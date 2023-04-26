1eaa5b2d93afd59ca5468e5b5fd70a08855c61db
[SW4/1반] 이준미 <junmi950918@gmail.com>
2023-04-26 21:57:00 +0900

    카테고리 수정버튼 기능 개발중

--- src/public/js/adminsCategory.js
+++ src/public/js/adminsCategory.js
@@ -8,7 +8,7 @@ if (categoryObj) {
       <td></td>
       <td>${obj.name}</td>
       <td>${obj.ex}</td>
-      <td><button class="btn-my-orders">수정</button></td>
+      <td><button id="edit" class="btn-my-orders">수정</button></td>
       <td><button class="btn-my-orders delete-btn" data-index="${index}">삭제</button></td>
     </tr>
   `).join("");
@@ -21,17 +21,29 @@ if (categoryObj) {
   deleteBtns.forEach(btn => {
     btn.addEventListener("click", function(e) {
       e.preventDefault();
-
-
+ 
       const index = parseInt(btn.dataset.index);
+      console.log(index)
 
-      // 해당 인덱스 삭제
+    // 해당 인덱스 삭제
       categoryObj.splice(index, 1);
       const categoryObjStr = JSON.stringify(categoryObj);
       localStorage.setItem("categoryObj", categoryObjStr);
 
-      
+
       btn.closest("tr").remove();
     });
   });
 }
+
+// 수정버튼 누르면 수정되기 ((실패.....다음에 도전))
+
+// const editBtn = document.querySelector("#edit");
+
+// editBtn.addEventListener("click" , function(event) {
+//   event.preventDefault();
+
+//   let dataEdit = JSON.parse(localStorage.getItem(categoryObj));
+//   console.log(dataEdit)
+
+// })
