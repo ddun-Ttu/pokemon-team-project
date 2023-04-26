9cce970e84aa43d0ec0f4c49bbd11a44270bdc49
buyeolim <buyeolim@gmail.com>
2023-04-27 02:04:39 +0900

    user, admin 라우터 html 연결 일부 확인 후 업데이트

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,42 +1,56 @@
 import express from "express";
 
-const userRouter = express.Router();
+const PATH_NAME = __dirname.split("/").slice(0, -1).join("/");
 
-// const PATH_NAME = __dirname.split("/").slice(0, -1).join("/");
+const userRouter = express.Router();
 
 // 회원가입 페이지
 userRouter.get("/signup", (req, res) => {
-  res.send("회원가입 페이지");
+  const PAGE_NAME = "signUp";
+
+  res.sendFile(`${PATH_NAME}/views/${PAGE_NAME}.html`);
 });
 
 // 로그인 페이지
 userRouter.get("/login", (req, res) => {
-  res.send("로그인 페이지");
+  const PAGE_NAME = "login";
+
+  res.sendFile(`${PATH_NAME}/views/${PAGE_NAME}.html`);
 });
 
 // 개인 페이지
 userRouter.get("/mypage", (req, res) => {
-  res.send("개인 페이지");
+  const PAGE_NAME = "myPage";
+
+  res.sendFile(`${PATH_NAME}/views/${PAGE_NAME}.html`);
 });
 
 // 사용자 확인 페이지
 userRouter.get("/mypage/confirm", (req, res) => {
-  res.send("사용자 확인 페이지");
+  const PAGE_NAME = "userAccount";
+
+  res.sendFile(`${PATH_NAME}/views/${PAGE_NAME}.html`);
 });
 
 // 사용자 정보 수정 페이지
 userRouter.get("/mypage/edit", (req, res) => {
-  res.send("사용자 정보 수정 페이지");
+  const PAGE_NAME = "?";
+
+  res.sendFile(`${PATH_NAME}/views/${PAGE_NAME}.html`);
 });
 
 // 사용자 주문 목록 조회 페이지
 userRouter.get("/mypage/orders", (req, res) => {
-  res.send("사용자 주문 목록 조회 페이지");
+  const PAGE_NAME = "orderStatus";
+
+  res.sendFile(`${PATH_NAME}/views/${PAGE_NAME}.html`);
 });
 
 // 사용자 주문 수정 페이지
 userRouter.get("/mypage/orders/:orderId/edit", (req, res) => {
-  res.send("사용자 주문 수정 페이지");
+  const PAGE_NAME = "?";
+
+  res.sendFile(`${PATH_NAME}/views/${PAGE_NAME}.html`);
 });
 
 export default userRouter;
