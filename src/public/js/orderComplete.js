99420f260009ef623aafb8e792870460458491bf
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-04-27 15:23:30 +0900

    메시지 박스 버튼 추가 및 링크 연결

---
+++ src/public/js/orderComplete.js
@@ -0,0 +1,12 @@
+const goOrderListButton = document.querySelector('#button-button1');
+
+console.log(common)
+
+goOrderListButton.addEventListener('click', goOrderListButtonHandler);
+
+async function goOrderListButtonHandler() {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  // * api(url: '/user.id', method: 'GET')
+  window.location = `/orderStaus/${user.id}`;
+}
