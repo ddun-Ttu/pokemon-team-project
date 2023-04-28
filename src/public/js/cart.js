c246c05882b42ddeb667efca7653d6c5c7b77884
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-04-28 13:11:29 +0900

    필드명 변경으로 발생했던 오류 수정. 장바구니 비었을 시 출력되는 메세지 박스 수정. 배송비 계산 방법 확정

--- src/public/js/cart.js
+++ src/public/js/cart.js
@@ -1,125 +1,123 @@
-const inCart = document.querySelector('.inCart-productList-ul');
-const paymentInformation = document.querySelector('.paymentInformation');
-const inCartAndpaymentInformationArea = document.querySelector('.inCartAndpaymentInformation');
+const inCart = document.querySelector(".inCart-productList-ul");
+const paymentInformation = document.querySelector(".paymentInformation");
+const inCartAndpaymentInformationArea = document.querySelector(
+  ".inCartAndpaymentInformation"
+);
 
 // * 더미 데이터.
 
 const dummyData = [
-  { 
+  {
+    pokemonId: 1,
     pokemonImage: `../img/${this.pokemonName}.png`,
-    pokemonName: '이상해씨',
+    pokemonName: "이상해씨",
     quantity: 1,
     pokemonPrice: 5000,
     checked: true,
 
-    pokemonId: 0,
     pokemonNum: 0,
-    sumInfo: '',
-    detailInfo: '',
-    categoryId: '풀',
-  },  
-  { 
-    pokemonImage: `../img/${this.pokemonName}.png`,
-    pokemonName: '피카츄',
-    quantity: 1,
-    pokemonPrice: 5000,
-    checked: true,
-
-    categoryId: '전기',
-    pokemonId: '',
-    pokemonNum: '',
-    sumInfo: '',
-    detailInfo: '',
-  }, 
-  { 
-    pokemonImage: `../img/${this.pokemonName}.png`,
-    pokemonName: '꼬부기',
-    quantity: 1,
-    pokemonPrice: 5000,
-    checked: true,
-
-    categoryId: '물',
-    pokemonId: 0,
-    pokemonNum: 0,
-    sumInfo: '',
-    detailInfo: '',
+    sumInfo: "",
+    detailInfo: "",
+    pokemonType: "풀",
   },
-  { 
+  {
+    pokemonId: 2,
     pokemonImage: `../img/${this.pokemonName}.png`,
-    pokemonName: '메타몽',
+    pokemonName: "피카츄",
     quantity: 1,
     pokemonPrice: 5000,
     checked: true,
 
-    categoryId: '말랑',
+    pokemonType: "전기",
+    pokemonNum: "",
+    sumInfo: "",
+    detailInfo: "",
+  },
+  {
+    pokemonId: 3,
+    pokemonImage: `../img/${this.pokemonName}.png`,
+    pokemonName: "꼬부기",
+    quantity: 1,
+    pokemonPrice: 5000,
+    checked: true,
+
+    pokemonType: "물",
+    pokemonNum: 0,
+    sumInfo: "",
+    detailInfo: "",
+  },
+  {
     pokemonId: 4,
+    pokemonImage: `../img/${this.pokemonName}.png`,
+    pokemonName: "메타몽",
+    quantity: 1,
+    pokemonPrice: 5000,
+    checked: true,
+
+    pokemonType: "말랑",
     pokemonNum: 4,
-    sumInfo: '',
-    detailInfo: '',
-  }, 
+    sumInfo: "",
+    detailInfo: "",
+  },
 ];
 
 // * 더미 데이터 삽입 버튼
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector('#button2');
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
 
-button1.addEventListener('click', () => {
-  let localStorageData = JSON.parse(localStorage.getItem('cart'));
+button1.addEventListener("click", () => {
+  let localStorageData = JSON.parse(localStorage.getItem("cart"));
 
-  if(localStorageData == null) {
+  if (localStorageData == null) {
     localStorageData = [];
 
     localStorageData.push(dummyData[0]);
-    
-    localStorage.setItem('cart', JSON.stringify(localStorageData));
-  }
-  else {
-    const index = 
-      localStorageData.findIndex(({ id }) => id == dummyData[0].id);
-      
-    if(index == -1) {
-      localStorageData.push(dummyData[0])
-    }
-    else {
+
+    localStorage.setItem("cart", JSON.stringify(localStorageData));
+  } else {
+    const index = localStorageData.findIndex(({ id }) => id == dummyData[0].id);
+
+    if (index == -1) {
+      localStorageData.push(dummyData[0]);
+    } else {
       localStorageData[index].quantity += 1;
     }
   }
-})
+});
 
-button2.addEventListener('click', () => {
-  let localStorageData = JSON.parse(localStorage.getItem('cart'));
+button2.addEventListener("click", () => {
+  let localStorageData = JSON.parse(localStorage.getItem("cart"));
 
-  if(localStorageData == null) {
+  if (localStorageData == null) {
     localStorageData = dummyData;
-    
-    localStorage.setItem('cart', JSON.stringify(localStorageData));    
-  }
-})
 
-printInCartAndPaymentInformation()
+    localStorage.setItem("cart", JSON.stringify(localStorageData));
+  }
+});
+
+printInCartAndPaymentInformation();
 
 function printInCartAndPaymentInformation() {
-  let localStorageData = JSON.parse(localStorage.getItem('cart'));
+  let localStorageData = JSON.parse(localStorage.getItem("cart"));
 
-  if(localStorageData == null) {
-    inCartAndpaymentInformationArea.innerHTML = '장바구니가 비었습니다.'
-    return
-  }
-  else{
-    let resultInCartHTML = '';
+  if (localStorageData == null) {
+    inCartAndpaymentInformationArea.innerHTML = `<div class="container-emptyCart"><div class="emptyCart">장바구니가 비었습니다.</div></div>`;
+    return;
+  } else {
+    let resultInCartHTML = "";
 
     makeInCartHTML();
 
     inCart.innerHTML = resultInCartHTML;
 
-    let resultPaymentInformationHTML = '';
-    
+    let resultPaymentInformationHTML = "";
+
     let totalProductCount = 0;
     let totalProductPrice = 0;
-    let deliveryFee = 3000;
+    let deliveryFee = 0;
 
     makePaymentInformationHTML();
-    
+
     paymentInformation.innerHTML = resultPaymentInformationHTML;
 
     // * inCartHTML 차례대로 배열에 넣기(루프 이용).
@@ -129,23 +127,21 @@ function printInCartAndPaymentInformation() {
       // 출력 횟수를 줄이는 게 좋으니까 변수에 할당하고 마지막에 innerHTML로 조작.
 
       localStorageData.forEach((item, index) => {
-        let { pokemonName, quantity, pokemonPrice, checked } = item;
-        
-        if(checked == true) {
-          checked = 'checked';
+        let { pokemonId, pokemonName, quantity, pokemonPrice, checked } = item;
+
+        if (checked == true) {
+          checked = "checked";
+        } else {
+          checked = "";
         }
-        else {
-          checked = '';
-        }
-        
-        resultInCartHTML += 
-          `
+
+        resultInCartHTML += `
           <li class="inCart-productList-li">
             <div class="container-productList-checkbox">
               <input class="productList-checkbox" type="checkbox" ${checked}>
             </div>
             <div class="container-productList-productIamge">
-            <img class="productList-productIamge" src="../img/${pokemonName}.png" alt="">
+            <a href='/pokemons/${pokemonId}' class="img"><img class="productList-productIamge" src="../img/${pokemonName}.png" alt=""></a>
             </div>
             <div class="container-productList-productNameAndCountHandle">
               <div class="container-productList-productName">
@@ -164,7 +160,7 @@ function printInCartAndPaymentInformation() {
               </div>
             </div>
             <div class="container-productList-productPrice">
-              <div>${pokemonPrice}</div>
+              <div>${pokemonPrice.toLocaleString()}</div>
             </div>
             <div class="container-productList-multiplication">
               <div class="productList-multiple">X</div>
@@ -176,174 +172,173 @@ function printInCartAndPaymentInformation() {
               <div class="productList-equal">=</div>
             </div>
             <div class="container-productList-productTotalPrice">
-              <div>${pokemonPrice * quantity}</div>
+              <div>${(pokemonPrice * quantity).toLocaleString()}</div>
             </div>
             <div class="container-productList-deleteButton">
               <button class="productList-deleteButton">삭제</button>
             </div>
           </li>
-          `
-      })
-    };
+          `;
+      });
+    }
 
     function makePaymentInformationHTML() {
-      localStorageData.forEach(item => {
+      localStorageData.forEach((item) => {
         let { pokemonPrice, quantity, checked } = item;
-        
-        if(checked == true) {
-        totalProductCount += quantity;
-        totalProductPrice += quantity * pokemonPrice;
+
+        if (checked == true) {
+          totalProductCount += quantity;
+          totalProductPrice += quantity * pokemonPrice;
+          deliveryFee += quantity * 5000;
         }
 
-        resultPaymentInformationHTML = 
-          `
+        resultPaymentInformationHTML = `
           <div class="container-paymentInformation-subject">
             <div class="paymentInformation-subject">결제 정보</div>
           </div>
           <div class="container-paymentInformation-productCount">
             <div class="paymentInformation-productCount1">상품 수</div>
-            <div class="paymentInformation-productCount2">${totalProductCount}</div>
+            <div class="paymentInformation-productCount2">${totalProductCount}개</div>
           </div>
           <div class="container-paymentInformation-productPrice">
             <div class="paymentInformation-productPrice1">상품 금액</div>
-            <div class="paymentInformation-productPrice2">${totalProductPrice}</div>
+            <div class="paymentInformation-productPrice2">${totalProductPrice.toLocaleString()}원</div>
           </div>
           <div class="container-paymentInformation-deliveryFee">
             <div class="paymentInformation-deliveryFee1">배송비</div>
-            <div class="paymentInformation-deliveryFee2">${deliveryFee}</div>
+            <div class="paymentInformation-deliveryFee2">${deliveryFee.toLocaleString()}원</div>
           </div>      
           <div class="container-paymentInformation-totalPrice">
             <div class="container-paymentInformation-totalPrice1">
               <div class="paymentInformation-totalPrice1">총 결제 금액</div>
             </div>
             <div class="container-paymentInformation-totalPrice2">
-              <div class="paymentInformation-totalPrice2">${totalProductPrice + deliveryFee}원</div>
+              <div class="paymentInformation-totalPrice2">${(
+                totalProductPrice + deliveryFee
+              ).toLocaleString()}원</div>
             </div>
           </div>     
           <div class="container-paymentInformation-paymentButton">
             <button class="paymentInformation-paymentButton">구매하기</button>
           </div>
           `;
-      })
-    };
+      });
+    }
   }
 
-  const minusButton = document.querySelectorAll('.minusButton');
-  const plusButton = document.querySelectorAll('.plusButton');
-  
+  const minusButton = document.querySelectorAll(".minusButton");
+  const plusButton = document.querySelectorAll(".plusButton");
+
   localStorageData.forEach((item, index) => {
-    minusButton[index].addEventListener('click', () => {
-      if(item.quantity == 1) {
-        alert('수량 설정은 1 이상만 가능합니다.')
+    minusButton[index].addEventListener("click", () => {
+      if (item.quantity == 1) {
+        alert("수량 설정은 1 이상만 가능합니다.");
+      } else {
+        item.quantity--;
       }
-      else{
-        item.quantity--;        
-      }
-      localStorage.setItem('cart', JSON.stringify(localStorageData));
-      printInCartAndPaymentInformation()
-    })
+      localStorage.setItem("cart", JSON.stringify(localStorageData));
+      printInCartAndPaymentInformation();
+    });
 
-    plusButton[index].addEventListener('click', () => {
+    plusButton[index].addEventListener("click", () => {
       item.quantity++;
-      localStorage.setItem('cart', JSON.stringify(localStorageData));
-      printInCartAndPaymentInformation()
-    })
-  })
+      localStorage.setItem("cart", JSON.stringify(localStorageData));
+      printInCartAndPaymentInformation();
+    });
+  });
 
-  const checkbox = document.querySelectorAll('.productList-checkbox');
-  const superCheckbox = document.querySelector('.head-selectAllAndcancleAllCheckbox');
-  
+  const checkbox = document.querySelectorAll(".productList-checkbox");
+  const superCheckbox = document.querySelector(
+    ".head-selectAllAndcancleAllCheckbox"
+  );
+
   let trueCheckboxCount = 0;
 
   localStorageData.forEach((item, index) => {
     const eachCheckbox = checkbox[index];
 
-    if(eachCheckbox.checked == true) {
-      trueCheckboxCount++
-      if(trueCheckboxCount == localStorageData.length) {
+    if (eachCheckbox.checked == true) {
+      trueCheckboxCount++;
+      if (trueCheckboxCount == localStorageData.length) {
         superCheckbox.checked = true;
-      }
-      else {
+      } else {
         superCheckbox.checked = false;
       }
     }
-    
-    eachCheckbox.addEventListener('click', () => {
-      if(eachCheckbox.checked == false) {
+
+    eachCheckbox.addEventListener("click", () => {
+      if (eachCheckbox.checked == false) {
         item.checked = false;
 
-        localStorage.setItem('cart', JSON.stringify(localStorageData));
-        printInCartAndPaymentInformation()
-      }
-      else if(eachCheckbox.checked == true) {
+        localStorage.setItem("cart", JSON.stringify(localStorageData));
+        printInCartAndPaymentInformation();
+      } else if (eachCheckbox.checked == true) {
         item.checked = true;
 
-        localStorage.setItem('cart', JSON.stringify(localStorageData));
-        printInCartAndPaymentInformation()
+        localStorage.setItem("cart", JSON.stringify(localStorageData));
+        printInCartAndPaymentInformation();
       }
-    })
-  })
+    });
+  });
 
-  superCheckbox.addEventListener('click', superCheckboxHandler);
-  
+  superCheckbox.addEventListener("click", superCheckboxHandler);
+
   function superCheckboxHandler() {
-    if(superCheckbox.checked == false) {
+    if (superCheckbox.checked == false) {
       superCheckbox.checked = false;
 
       localStorageData.forEach((item, index) => {
         item.checked = false;
         checkbox[index].checked = false;
-      })
-    }
-    else if(superCheckbox.checked == true) {
+      });
+    } else if (superCheckbox.checked == true) {
       superCheckbox.checked = true;
 
       localStorageData.forEach((item, index) => {
         item.checked = true;
         checkbox[index].checked = true;
-      })
+      });
     }
 
-    localStorage.setItem('cart', JSON.stringify(localStorageData));
-    
-    printInCartAndPaymentInformation()
+    localStorage.setItem("cart", JSON.stringify(localStorageData));
+
+    printInCartAndPaymentInformation();
   }
 
-  const deleteButton = document.querySelectorAll('.productList-deleteButton');
+  const deleteButton = document.querySelectorAll(".productList-deleteButton");
 
   localStorageData.forEach((item, index) => {
     const eachDeleteButton = deleteButton[index];
 
-    eachDeleteButton.addEventListener('click', (e) => {
+    eachDeleteButton.addEventListener("click", (e) => {
       const target = e.target;
 
-      if(localStorageData.length == 1) {
-        localStorage.removeItem('cart');
-      }
-      else{
+      if (localStorageData.length == 1) {
+        localStorage.removeItem("cart");
+      } else {
         localStorageData.splice(index, 1);
-        localStorage.setItem('cart', JSON.stringify(localStorageData))
+        localStorage.setItem("cart", JSON.stringify(localStorageData));
       }
 
-      printInCartAndPaymentInformation()
-    })
+      printInCartAndPaymentInformation();
+    });
   });
 
-  const deleteAllButton = document.querySelector('.head-deleteAllButton');
+  const deleteAllButton = document.querySelector(".head-deleteAllButton");
 
-  deleteAllButton.addEventListener('click', () => {
-    localStorage.removeItem('cart')
+  deleteAllButton.addEventListener("click", () => {
+    localStorage.removeItem("cart");
 
-    printInCartAndPaymentInformation()
+    printInCartAndPaymentInformation();
   });
 
-  const goOrderButton = document.querySelector('.paymentInformation-paymentButton');
+  const goOrderButton = document.querySelector(
+    ".paymentInformation-paymentButton"
+  );
 
-  goOrderButton.addEventListener('click', goOrderButtonHandler);
+  goOrderButton.addEventListener("click", goOrderButtonHandler);
 
   async function goOrderButtonHandler() {
-  // 주문 페이지로 보내기 전에 먼저 작업해줄 게 있을까?
-  
-    window.location.href="./orderAndPayment.html";
+    window.location.href = "./orderAndPayment.html";
   }
-};
+}
