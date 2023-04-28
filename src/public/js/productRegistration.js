ce265e5a8269928b1f1666400eef7f2e8b8e51ee
buyeolim <buyeolim@gmail.com>
2023-04-28 02:31:01 +0000

    포켓몬(상품) 이미지를 제외 한 데이터 db 전달 확인

--- src/public/js/productRegistration.js
+++ src/public/js/productRegistration.js
@@ -1,47 +1,123 @@
+// const e = require("express");
 
 const RegisterProduct = document.querySelector("#but-sing-up");
-const photoUpload = document.getElementById('photo-upload');
-const preview = document.getElementById('preview');
+const photoUpload = document.getElementById("photo-upload");
+const preview = document.getElementById("preview");
 
 // 사진 등록
-photoUpload.addEventListener('change', function() {
-    const file = this.files[0];
-    const reader = new FileReader();
-  
-    reader.addEventListener('load', function() {
-      preview.src = reader.result;
-    });
-  
-    reader.readAsDataURL(file);
+photoUpload.addEventListener("change", function () {
+  const file = this.files[0];
+  const reader = new FileReader();
 
-    console.log(file);
+  reader.addEventListener("load", function () {
+    preview.src = reader.result;
   });
 
+  reader.readAsDataURL(file);
+
+  console.log(file);
+});
 
 // 인풋값 확인
 
-function checkInput(ev) {
-    ev.preventDefault();
-    const inputCh1 = document.querySelector("#prodeuct-name");
-    const inputCh2 = document.querySelector("#items");
-    const inputCh3 = document.querySelector("#summary");
-    const inputCh4 = document.querySelector("#detailed-description");
-    const inputCh5 = document.querySelector("#inventory");
-    const inputCh6 = document.querySelector("#price");
-    
-    console.log(inputCh1.value);
-    console.log(inputCh2.value);
-    console.log(inputCh3.value);
-    console.log(inputCh4.value);
-    console.log(inputCh5.value);
-    console.log(inputCh6.value);
+// function checkInput(ev) {
+//     ev.preventDefault();
+//     const inputCh1 = document.querySelector("#prodeuct-name");
+//     const inputCh2 = document.querySelector("#items");
+//     const inputCh3 = document.querySelector("#summary");
+//     const inputCh4 = document.querySelector("#detailed-description");
+//     const inputCh5 = document.querySelector("#inventory");
+//     const inputCh6 = document.querySelector("#price");
 
-    // 카테고리 확인
-    const select = document.getElementById("items");
-    select.addEventListener("change", function() {
-      const selectedValue = select.value;
-      console.log(selectedValue);
-    });
-}
+//     console.log(inputCh1.value);
+//     console.log(inputCh2.value);
+//     console.log(inputCh3.value);
+//     console.log(inputCh4.value);
+//     console.log(inputCh5.value);
+//     console.log(inputCh6.value);
 
-RegisterProduct.addEventListener("click", checkInput);
+//     // 카테고리 확인
+//     const select = document.getElementById("items");
+//     select.addEventListener("change", function() {
+//       const selectedValue = select.value;
+//       console.log(selectedValue);
+//     });
+// }
+
+// RegisterProduct.addEventListener("click", checkInput);
+
+// 제품 추가 페이지의 필드에 데이터를 쓰고 req.body에 넣어 JSON으로 변환한다"는 것은 사용자가 입력한 데이터가 양식 필드에서 수집되어 req에 저장된다는 것을 의미합니다
+
+const inputCh1 = document.querySelector("#prodeuct-name"); // 상품명
+const inputCh2 = document.querySelector("#items"); // 카테고리
+const inputCh3 = document.querySelector("#summary"); // 설명
+// const inputCh4 = document.querySelector("#detailed-description"); // ?
+// const inputCh5 = document.querySelector("#inventory"); // 번호
+const inputCh6 = document.querySelector("#price"); // 가격
+const photo = document.querySelector("#photo-upload"); // 사진
+
+RegisterProduct.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  // 인풋에 들어온 값 상수에 담기
+  const productNameInput = inputCh1.value;
+  const itemsInput = inputCh2.value;
+  const summaryInput = inputCh3.value;
+  // const detailedDescriptionInput = inputCh4.value;
+  // const inventoryInput = inputCh5.value;
+  const priceInput = inputCh6.value;
+  const photoInput = photo.value;
+
+  console.log({
+    productNameInput,
+    itemsInput,
+    summaryInput,
+    priceInput,
+    photoInput,
+  });
+
+  // 로컬에서 데이터 가져옴
+  const existingCategoryObjStr = localStorage.getItem("registratObj");
+  // let existingCategoryObj = [];
+  // 로컬 데이터가 null이 아닐경우 실행
+  if (existingCategoryObjStr !== null) {
+    existingCategoryObj = JSON.parse(existingCategoryObjStr);
+  }
+
+  //인풋값에 담기는 정보 상수에 담기
+  const categoryObj = {
+    pokemonName: productNameInput,
+    pokemonType: itemsInput,
+    detailInfo: summaryInput,
+    // detailedDescription: detailedDescriptionInput,
+    // inventory: inventoryInput,
+    price: priceInput,
+    pokemonImage: photoInput,
+  };
+  // existingCategoryObj.push(categoryObj);
+
+  // 위에서 담은 상수를 JSON 후 setitem으로 담기
+  // const updatedCategoryObjStr = JSON.stringify(existingCategoryObj);
+  // const updatedCategoryObjStr = JSON.stringify(categoryObj);
+  // localStorage.setItem("registratObj", updatedCategoryObjStr);
+
+  console.log(categoryObj);
+
+  // [POST] /api/pokemons 요청
+  await fetch(common.API_URL + "/api/pokemons", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(categoryObj),
+  });
+
+  /*
+  inputCh1.value = "";
+  inputCh2.value = "전체";
+  inputCh3.value = "";
+  // inputCh4.value = "";
+  // inputCh5.value = "";
+  inputCh6.value = "";
+  */
+});
