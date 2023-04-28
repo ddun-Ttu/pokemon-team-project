9b51fec868afcee1e688e56e5989ece295c0acbc
[SW4/1반] 이준미 <junmi950918@gmail.com>
2023-04-28 14:35:05 +0900

    오탈자 수정

--- src/public/js/adminsCategory.js
+++ src/public/js/adminsCategory.js
@@ -1,10 +1,7 @@
 const localCategory = localStorage.getItem("categoryObj");
 const categoryObj = JSON.parse(localCategory);
 
-<<<<<<< HEAD
 // 카테고리 목록 추가
-=======
->>>>>>> develop
 if (categoryObj) {
   const categoryHtml = categoryObj
     .map(
@@ -13,13 +10,7 @@ if (categoryObj) {
       <td></td>
       <td>${obj.name}</td>
       <td>${obj.ex}</td>
-<<<<<<< HEAD
       <td><button id="edit" class="btn-my-orders">수정</button></td>
-=======
-      <td>
-          <button class="btn-my-orders">수정</button>
-      </td>
->>>>>>> develop
       <td><button class="btn-my-orders delete-btn" data-index="${index}">삭제</button></td>
     </tr>
   `
@@ -29,19 +20,16 @@ if (categoryObj) {
   const listCategory = document.querySelector("#list-category");
   listCategory.innerHTML = categoryHtml;
 
-<<<<<<< HEAD
   // 삭제버튼 누르면 요소 삭제
-=======
->>>>>>> develop
   const deleteBtns = document.querySelectorAll(".delete-btn");
   deleteBtns.forEach((btn) => {
     btn.addEventListener("click", function (e) {
       e.preventDefault();
- 
-      const index = parseInt(btn.dataset.index);
-      console.log(index)
 
-    // 해당 인덱스 삭제
+      const index = parseInt(btn.dataset.index);
+      console.log(index);
+
+      // 해당 인덱스 삭제
       categoryObj.splice(index, 1);
       const categoryObjStr = JSON.stringify(categoryObj);
       localStorage.setItem("categoryObj", categoryObjStr);
@@ -50,7 +38,6 @@ if (categoryObj) {
     });
   });
 }
-<<<<<<< HEAD
 
 // 수정버튼 누르면 수정되기 ((실패.....다음에 도전))
 
@@ -63,5 +50,3 @@ if (categoryObj) {
 //   console.log(dataEdit)
 
 // })
-=======
->>>>>>> develop
