98109da1b94531df27322ad6c12f558891f1c74a
ungbi <zo0qzzz@gmail.com>
2023-05-12 04:45:40 +0900

    주소 찾기 버튼 수리 & 테스트 레이아웃 작성성

--- src/public/js/order.js
+++ src/public/js/order.js
@@ -150,37 +150,7 @@ searchAddressButton.addEventListener('click', searchAddress);
 
 function searchAddress(e) {
   e.preventDefault();
-  // new daum.Postcode({
-  //   oncomplete: function (data) {
-  //     let addr = '';
-  //     let extraAddr = '';
 
-  //     if (data.userSelectedType === 'R') {
-  //       addr = data.roadAddress;
-  //     } else {
-  //       addr = data.jibunAddress;
-  //     }
-
-  //     if (data.userSelectedType === 'R') {
-  //       if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
-  //         extraAddr += data.bname;
-  //       }
-  //       if (data.buildingName !== '' && data.apartment === 'Y') {
-  //         extraAddr +=
-  //           extraAddr !== '' ? ', ' + data.buildingName : data.buildingName;
-  //       }
-  //       if (extraAddr !== '') {
-  //         extraAddr = ' (' + extraAddr + ')';
-  //       }
-  //     } else {
-  //     }
-
-  //     postalCodeInput.value = data.zonecode;
-  //     address1Input.value = `${addr} ${extraAddr}`;
-  //     address2Input.placeholder = '상세 주소를 입력해 주세요.';
-  //     address2Input.focus();
-  //   },
-  // }).open();
   new daum.Postcode({
     oncomplete: function (data) {
       // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
