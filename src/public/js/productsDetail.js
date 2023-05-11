1e2406bac9cc69403d73fa699e99ccd9b6fa5413
ungbi <zo0qzzz@gmail.com>
2023-05-11 17:40:47 +0900

    상세 페이지 수리 완료

--- src/public/js/productsDetail.js
+++ src/public/js/productsDetail.js
@@ -3,21 +3,110 @@ const putInCartButton = document.querySelector('.description-cart');
 const orderNowButton = document.querySelector('.description-order');
 const countInput = document.querySelector('#count');
 const pathname = window.location.pathname;
+
+// 더미 데이터)
+
+const dummy = {
+  categoryNameData: [
+    { main: '포켓몬', sub: ['물', '불', '풀', '전기'] },
+    { main: '몬스터볼' },
+    { main: '사료' },
+    { main: '진화의 돌' },
+    { main: '악세서리' },
+    { main: '인형' },
+  ],
+  productData: [
+    {
+      _id: 1,
+      img: 'url',
+      name: '꼬부기',
+      price: 1000,
+      categoryName: '포켓몬',
+      description: '긍지 높은 포켓몬',
+      stock: 20,
+    },
+    {
+      _id: 2,
+      img: 'url',
+      name: '메타몽',
+      price: 1000,
+      categoryName: '포켓몬',
+      description: '긍지 높은 포켓몬',
+      stock: 20,
+    },
+    {
+      _id: 3,
+      img: 'url',
+      name: '이상해씨',
+      price: 1000,
+      categoryName: '포켓몬',
+      description: '긍지 높은 포켓몬',
+      stock: 20,
+    },
+    {
+      _id: 4,
+      img: 'url',
+      name: '피카츄',
+      price: 1000,
+      categoryName: '포켓몬',
+      stock: 0,
+    },
+    {
+      _id: 5,
+      img: 'url',
+      name: '몬스터볼',
+      price: 1000,
+      categoryName: '몬스터볼',
+      stock: 20,
+    },
+    {
+      _id: 6,
+      img: 'url',
+      name: '메타몽',
+      price: 1000,
+      categoryName: '포켓몬',
+      stock: 0,
+    },
+    {
+      _id: 7,
+      img: 'url',
+      name: '몬스터볼',
+      price: 1000,
+      categoryName: '몬스터볼',
+      stock: 20,
+    },
+    {
+      _id: 8,
+      img: 'url',
+      name: '피카츄',
+      price: 1000,
+      categoryName: '포켓몬',
+      stock: 20,
+    },
+  ],
+};
+
+let productData = dummy.productData;
+
+const _id = pathname.split('/')[2];
+
+const findedItem = productData.find(item => item._id === Number(_id));
+
+const data = findedItem;
+
+let { img, name, categoryName, price, description, stock } = data;
+
 makeDetail();
 
 async function makeDetail() {
-  const res = await fetch(`${common.API_URL}/api${pathname}`);
-  let data = await res.json();
+  // const res = await fetch(`${common.API_URL}/api${pathname}`);
+  // let data = await res.json();
 
-  let { _id, pokemonImage, pokemonName, pokemonType, price, detailInfo } = data;
-
-  console.log(data);
-
-  pricetoLocaleString = Number(price).toLocaleString();
+  // pricetoLocaleString = Number(price).toLocaleString();
 
   let typeColor;
 
-  switch (pokemonType) {
+  switch (categoryName) {
     case '물':
       typeColor = 'rgb(41, 146, 255)';
       break;
@@ -32,30 +121,37 @@ async function makeDetail() {
   const detailImage = document.querySelector('.container-detail-image');
   const detailDescriptionOne = document.querySelector('.one');
 
-  detailImage.innerHTML = `
-<div class="detail-image">
-  <img class="detail-image-img" src=${pokemonImage} onerror="this.onerror=null; this.src='../img/피카츄.png';" alt="">
-</div>
-`;
+  const detailImageHTML = `
+    <div class="detail-image">
+      <img class="detail-image-img" src=${img} onerror="this.onerror=null; this.src='/imgs/모래두지.png';" alt=""></img>
+    </div>
+    `;
 
-  detailDescriptionOne.innerHTML = `
-<div class="detail-description">
-  <div class="container-description-nameAndType">
-    <div class="container-description-name">
-      <div class="description-name">${pokemonName}</div>
+  detailImage.insertAdjacentHTML('beforeend', detailImageHTML);
+
+  const detailDescriptionOneHTML = `
+    <div class="detail-description">
+      <div class="container-description-nameAndType">
+        <div class="container-description-name">
+          <div class="description-name">${name}</div>
+        </div>
+        <div class="container-description-type">
+          <div class="description-type" style="background-color: ${typeColor}">${categoryName}</div>
+        </div>
+      </div>
+      <div class="container-description-price">
+        <div class="description-price">${price.toLocaleString()}원</div>
+      </div>
+      <div class="container-description-description">
+        <div class="description-description">${description}</div>
+      </div>
     </div>
-    <div class="container-description-type">
-      <div class="description-type" style="background-color: ${typeColor}">${pokemonType}</div>
-    </div>
-  </div>
-  <div class="container-description-price">
-    <div class="description-price">${pricetoLocaleString}원</div>
-  </div>
-  <div class="container-description-description">
-    <div class="description-description">${detailInfo}</div>
-  </div>
-</div>
-`;
+    `;
+
+  detailDescriptionOne.insertAdjacentHTML(
+    'beforeend',
+    detailDescriptionOneHTML,
+  );
 
   putInCartButton.addEventListener('click', putInCartButtonHandler);
 
@@ -68,7 +164,7 @@ async function makeDetail() {
 
     let data = {
       _id,
-      pokemonName,
+      name,
       price,
     };
 
@@ -84,9 +180,7 @@ async function makeDetail() {
 
       localStorage.setItem('cart', JSON.stringify(alreadyInCartData));
     } else {
-      const findedIndex = alreadyInCartData.findIndex(
-        item => item.pokemonName == pokemonName,
-      );
+      const findedIndex = alreadyInCartData.findIndex(item => item._id == _id);
 
       if (alreadyInCartData !== null && findedIndex == -1) {
         data.quantity = count;
@@ -109,7 +203,7 @@ async function makeDetail() {
     let data = [
       {
         _id,
-        pokemonName,
+        name,
         price,
         quantity,
       },
@@ -117,6 +211,6 @@ async function makeDetail() {
 
     localStorage.setItem('order', JSON.stringify(data));
 
-    window.location = '/orders';
+    window.location = '/order';
   }
 }
