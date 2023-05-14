8f684fc857e043021b8e9affecdcd34efda4a3ec
alloboo <hye870425@gmail.com>
2023-05-14 22:26:41 +0900

    fetch 요청 코드 및 성공 시 redirect 코드 추가

--- src/public/js/usersMyConfirm.js
+++ src/public/js/usersMyConfirm.js
@@ -1,36 +1,52 @@
 const passwordInput = document.querySelector('#account-password');
-const confirmPasswordInput = document.querySelector('#account-confirmPassword');
 const password = document.querySelector('#password');
-const confirmPassword = document.querySelector('#confirmPassword');
-const saveButton = document.querySelector('#saveButton');
-
-// a태그 안에 button태그가 함께 있어서 링크 연결이 안 되는 부분을 해결하는 코드
-saveButton.addEventListener('click', function (e) {
-  e.preventDefault();
-  const url = document.querySelector('.link-a').getAttribute('href');
-  window.location.href = url;
-});
+const confirmBtn = document.querySelector('#confirmButton');
 
 // 패스워드가 8글자인지 확인
 passwordInput.addEventListener('input', function () {
   console.log(passwordInput.value.length);
   const passwordLength = passwordInput.value.length;
 
-  if (passwordLength < 9) {
+  if (passwordLength < 8) {
     password.classList.remove('block');
   } else {
     password.classList.add('block');
   }
 });
 
-// 패스워드가 동일한지
+confirmBtn.addEventListener('click', async e => {
+  e.preventDefault();
 
-confirmPasswordInput.addEventListener('input', function () {
-  console.log(confirmPasswordInput.value);
-  console.log(passwordInput.value);
-  if (confirmPasswordInput.value !== passwordInput.value) {
-    confirmPassword.classList.remove('block');
+  const userId = localStorage.getItem('userId');
+  const token = localStorage.getItem('token');
+
+  await fetch(API_URL + `/api/users/${userId}`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ password: passwordInput.value }),
+  })
+    .then(res => res.json())
+    .then(data => {
+      dataObj = data;
+    });
+
+  const result = dataObj.ok;
+
+  if (!result || result === 'false') {
+    alert('올바르지 않은 접근입니다.');
+    window.location.replace('/');
+    return;
   } else {
-    confirmPassword.classList.add('block');
+    console.log('올바른 접근입니다.');
+
+    let urlArr = window.location.href.split('/');
+    urlArr.pop();
+    urlArr.push('edit');
+    window.location.href = urlArr.join('/');
+    return;
   }
 });
