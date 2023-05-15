dec0a712550b1bf6cdaa4db92b3ea83c10d2a879
zo0ozzz <zo0qzzz@gmail.com>
2023-05-15 17:57:36 +0900

    중간 저장

--- src/public/js/productsDetail.js
+++ src/public/js/productsDetail.js
@@ -1,90 +1,195 @@
-const detail = document.querySelector('.detail');
-const putInCartButton = document.querySelector('.description-cart');
-const orderNowButton = document.querySelector('.description-order');
-const countInput = document.querySelector('#count');
-const pathname = window.location.pathname;
+// const detail = document.querySelector('.detail');
+// const putInCartButton = document.querySelector('.description-cart');
+// const orderNowButton = document.querySelector('.description-order');
+// const countInput = document.querySelector('#count');
 
-// 더미 데이터)
+makeProductDetail();
 
-const dummy = {
-  categoryNameData: [
-    { main: '포켓몬', sub: ['물', '불', '풀', '전기'] },
-    { main: '몬스터볼' },
-    { main: '사료' },
-    { main: '진화의 돌' },
-    { main: '악세서리' },
-    { main: '인형' },
-  ],
-  productData: [
-    {
-      _id: 1,
-      img: 'url',
-      name: '꼬부기',
-      price: 1000,
-      categoryName: '포켓몬',
-      description: '긍지 높은 포켓몬',
-      stock: 20,
-    },
-    {
-      _id: 2,
-      img: 'url',
-      name: '메타몽',
-      price: 1000,
-      categoryName: '포켓몬',
-      description: '긍지 높은 포켓몬',
-      stock: 20,
-    },
-    {
-      _id: 3,
-      img: 'url',
-      name: '이상해씨',
-      price: 1000,
-      categoryName: '포켓몬',
-      description: '긍지 높은 포켓몬',
-      stock: 20,
-    },
-    {
-      _id: 4,
-      img: 'url',
-      name: '피카츄',
-      price: 1000,
-      categoryName: '포켓몬',
-      stock: 0,
-    },
-    {
-      _id: 5,
-      img: 'url',
-      name: '몬스터볼',
-      price: 1000,
-      categoryName: '몬스터볼',
-      stock: 20,
-    },
-    {
-      _id: 6,
-      img: 'url',
-      name: '메타몽',
-      price: 1000,
-      categoryName: '포켓몬',
-      stock: 0,
-    },
-    {
-      _id: 7,
-      img: 'url',
-      name: '몬스터볼',
-      price: 1000,
-      categoryName: '몬스터볼',
-      stock: 20,
-    },
-    {
-      _id: 8,
-      img: 'url',
-      name: '피카츄',
-      price: 1000,
-      categoryName: '포켓몬',
-      stock: 20,
-    },
-  ],
-};
+function makeProductDetail() {
+  const _id = Number(getProduct_id());
+  const url = makeUrlToGetProductData(_id);
+
+  getProductData(url)
+    .then(([res]) => {
+      const res1 = makeDatailImageHTML(res);
+      const res2 = makeDetailDescriptionOneHTML(res);
+
+      return [res1, res2];
+    })
+    .then(([res1, res2]) => {
+      insertDetailImageHTML(res1);
+      insertDetailDescriptionOneHTML(res2);
+    })
+    .then(() => {});
+
+  function getProduct_id() {
+    const pathname = window.location.pathname;
+    const _id = pathname.split('/')[2];
+
+    return _id;
+  }
+
+  function makeUrlToGetProductData(product_id) {
+    const url = `http://api/products/${product_id}`;
+
+    return url;
+  }
+
+  async function getProductData(url) {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      return data;
+    } catch (error) {
+      console.log(
+        `error! at getProductData(). data is replaced dummy data.\n${error}`,
+      );
+
+      const data = dummy.getSelectedProductData('_id', _id);
+      console.log(data);
+      return data;
+    }
+  }
+
+  function makeDatailImageHTML(productData) {
+    let { img } = productData;
+
+    const detailImageHTML = `
+    <div class="detail-image">
+      <img class="detail-image-img" src=${img} onerror="this.onerror=null; this.src='/imgs/모래두지.png';" alt=""></img>
+    </div>
+    `;
+
+    return detailImageHTML;
+  }
+
+  function makeDetailDescriptionOneHTML(productData) {
+    const { name, categoryName, price, description, stock } = productData;
+
+    const detailDescriptionOneHTML = `
+    <div class="detail-description">
+      <div class="container-description-nameAndType">
+        <div class="container-description-name">
+          <div class="description-name">${name}</div>
+        </div>
+        <div class="container-description-type">
+          <div class="description-type" style="display: 'none'">${categoryName}</div>
+        </div>
+      </div>
+      <div class="container-description-price">
+        <div class="description-price">${price.toLocaleString()}원</div>
+      </div>
+      <div class="container-description-description">
+        <div class="description-description">${description}</div>
+      </div>
+    </div>
+    `;
+
+    return detailDescriptionOneHTML;
+  }
+
+  function insertDetailImageHTML(detailImageHTML) {
+    const detailImage = document.querySelector('.container-detail-image');
+
+    detailImage.insertAdjacentHTML('beforeend', detailImageHTML);
+  }
+
+  function insertDetailDescriptionOneHTML(detailDescriptionOneHTML) {
+    const detailDescriptionOne = document.querySelector('.one');
+
+    detailDescriptionOne.insertAdjacentHTML(
+      'beforeend',
+      detailDescriptionOneHTML,
+    );
+  }
+
+  function addEventListenerToAddProductToCartButton() {
+    const addProductToCartButton = document.querySelector('.description-cart');
+
+    addProductToCartButton.addEventListener('click', addProductToCart);
+  }
+
+  function addProductToCart() {
+
+    function addEventListenerToElement(element) {
+      element.addEventListener('click', e => {
+        let { _id, name, price } = e.target.dataset;
+        _id = Number(_id);
+
+        addProudctToCart(_id, name, price);
+      });
+    }
+
+    function addProudctToCart(_id, name, price) {
+      let cartData = getCartData();
+
+      const productData = { _id, name, price };
+      const productDataForCart = convertProductDataForCart(productData);
+
+      if (!isCart()) {
+        createCartStorage(productDataForCart);
+
+        return;
+      }
+
+      const alreadyInCartDataIndex = cartData.findIndex(
+        item => item._id === _id,
+      );
+
+      if (alreadyInCartDataIndex === -1) {
+        cartData.push(productDataForCart);
+
+        const updatedCartData = cartData;
+
+        setCartStorage(updatedCartData);
+
+        return;
+      }
+
+      cartData[alreadyInCartDataIndex].quantity++;
+
+      const updatedCartData = cartData;
+
+      setCartStorage(updatedCartData);
+
+      return;
+    }
+
+    function getCartData() {
+      let cartData = JSON.parse(localStorage.getItem('cart'));
+
+      return cartData;
+    }
+
+    function convertProductDataForCart(productData) {
+      const { _id, name, price, quantity = 1, checked = true } = productData;
+
+      const productDataForCart = { _id, name, price, quantity, checked };
+
+      return productDataForCart;
+    }
+
+    function isCart() {
+      const cartData = JSON.parse(localStorage.getItem('cart'));
+      if (cartData === null) {
+        return false;
+      }
+
+      return true;
+    }
+
+    function createCartStorage(firstProductDataForCart) {
+      localStorage.setItem('cart', JSON.stringify([firstProductDataForCart]));
+    }
+
+    function setCartStorage(productDataForCart) {
+      localStorage.setItem('cart', JSON.stringify(productDataForCart));
+    }
+  }
+  
+  }
+}
 
 // let productData = dummy.productData;
 
@@ -121,13 +226,10 @@ async function makeDetail() {
   const detailImage = document.querySelector('.container-detail-image');
   const detailDescriptionOne = document.querySelector('.one');
 
-  const detailImageHTML = `
-    <div class="detail-image">
-      <img class="detail-image-img" src=${img} onerror="this.onerror=null; this.src='/imgs/모래두지.png';" alt=""></img>
-    </div>
-    `;
-
-  detailImage.insertAdjacentHTML('beforeend', detailImageHTML);
+  const detailImageHTML = detailImage.insertAdjacentHTML(
+    'beforeend',
+    detailImageHTML,
+  );
 
   const detailDescriptionOneHTML = `
     <div class="detail-description">
